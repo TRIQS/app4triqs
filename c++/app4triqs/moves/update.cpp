d19ecd73cbaac9d9d2f34e4e417a0d34aef7c260
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-10-31 16:26:59 -0400

    Template project for a TRIQS based imaginary time QMC impurity solver

---
+++ c++/app4triqs/moves/update.cpp
@@ -0,0 +1,19 @@
+#include "./update.hpp"
+
+namespace app4triqs::moves {
+
+  mc_weight_t insert::attempt() {
+    // TODO
+    return {};
+  }
+
+  mc_weight_t insert::accept() {
+    // TODO
+    return {};
+  }
+
+  void insert::reject() {
+    // TODO
+  }
+
+} // namespace app4triqs::moves
