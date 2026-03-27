9a13c5157590b2a0735fb64982808cdb0fbf7e1b
Hugo U. R. Strand <hugo.strand@gmail.com>
2026-03-27 08:08:13 +0100

    adhere to c2py arithmetic tuple pair api change

--- python/app4triqs/module.cpp
+++ python/app4triqs/module.cpp
@@ -4,6 +4,6 @@
 
 using namespace std::string_literals;
 using app4triqs::toto;
-template <> struct c2py::arithmetic<toto, c2py::OpName::Add> : std::tuple<triplet<toto, toto, toto>> {};
+template <> struct c2py::arithmetic<toto, c2py::OpName::Add> : std::tuple<std::pair<toto, toto>> {};
 
 #include "module.wrap.cxx"
